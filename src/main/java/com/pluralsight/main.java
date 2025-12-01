a1586a498933025b3b8c210a92942b1b3afc5473
Romell Walker <walkerromell@gmail.com>
2025-12-01 14:50:55 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/main.java
@@ -0,0 +1,33 @@
+package com.pluralsight;
+
+import java.sql.*;
+
+public class main {
+    public static void main(String[] args) throws SQLException {
+
+        String url = "jdbc:mysql://localhost:3306/northwind";
+        String username = "root";
+        String password = "";
+
+        Connection connection = DriverManager.getConnection(url, username, password);
+        // create statement
+        // the statement is tied to the open connection
+        Statement statement = connection.createStatement();
+
+        //define your query
+        String query = "SELECT ProductName FROM Products ";
+        // 2. Execute your query
+        ResultSet results = statement.executeQuery(query);
+        // process the results
+        while (results.next()) {
+            String products = results.getString("ProductName");
+            System.out.println(products);
+        }
+        // 3. Close the connection
+        connection.close();
+        System.out.println(results);
+
+
+
+    }
+}
